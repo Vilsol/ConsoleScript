d9515d8a662599099bb435a1dfcdcdc58573df60
Vilsol <vilsol2000@gmail.com>
2013-10-15 21:08:45 +0100

    Now mobs can drop multiple of the same item!

--- src/main/java/consolescript/ConsoleScriptListener.java
+++ src/main/java/consolescript/ConsoleScriptListener.java
@@ -16,14 +16,19 @@ public class ConsoleScriptListener implements Listener {
 		if(ConsoleScript.customEntities.containsKey(evt.getEntity().getEntityId())){
 			Location dropLocation = evt.getEntity().getLocation().add(new Vector(0, 1, 0));
 			for(String s : ConsoleScript.customEntities.get(evt.getEntity().getEntityId())){
-				if(Utils.isInteger(s)){
-					if(Material.getMaterial(Integer.parseInt(s)) != null){
-						ItemStack i = new ItemStack(Integer.parseInt(s), 1);
+				String item = s.split(" ")[0];
+				String amount = s.split(" ")[1];
+				if(Utils.isInteger(item)){
+					if(Material.getMaterial(Integer.parseInt(item)) != null){
+						ItemStack i = new ItemStack(Integer.parseInt(item), 1);
+						if(amount != null && Utils.isInteger(amount)) i.setAmount(Integer.parseInt(amount));
 						if(i != null) evt.getEntity().getWorld().dropItem(dropLocation, i);
 					}
-				}else if(Utils.isCustomItem(s)){
-					CustomItem ci = new CustomItem(s);
-					evt.getEntity().getWorld().dropItem(dropLocation, ci.getItemStack());
+				}else if(Utils.isCustomItem(item)){
+					CustomItem ci = new CustomItem(item);
+					ItemStack cu = ci.getItemStack();
+					if(amount != null && Utils.isInteger(amount)) cu.setAmount(Integer.parseInt(amount));
+					evt.getEntity().getWorld().dropItem(dropLocation, cu);
 				}
 			}
 			ConsoleScript.customEntities.remove(evt.getEntity().getEntityId());
